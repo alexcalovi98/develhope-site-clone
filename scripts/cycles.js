33f3e4a1064b4b4ea49fc0d4f9b66e2ba3dcee0d
Alex Calovi <alex.calovi98@gmail.com>
2026-04-07 19:21:08 +0200

    added cycles.js: while, do-while and for

---
+++ scripts/cycles.js
@@ -0,0 +1,49 @@
+// Generate a number between 4 and 10
+const minVote = 4;
+const maxVote = 10;
+
+// while, do-while, for
+let generatedVote
+
+// While
+generatedVote = Math.floor(Math.random() * (maxVote - minVote + 1)) + minVote
+console.log("[OUTSIDE WHILE] The student vote is:", generatedVote);
+while (generatedVote < 6) {
+    console.log("The student vote is INSUFFICIENT");
+    generatedVote = Math.floor(Math.random() * (maxVote - minVote + 1)) + minVote;
+    console.log("[WHILE] The student vote is:", generatedVote);
+}
+console.log("The student vote is SUFFICIENT");
+
+// Refactor with do-while
+do {
+    generatedVote = Math.floor(Math.random() * (maxVote - minVote + 1)) + minVote
+    console.log("[DO WHILE] The student vote is:", generatedVote)
+    if (generatedVote < 6) {
+        console.log("The student vote is INSUFFICIENT")
+    }
+} while (generatedVote < 6)
+ console.log("The student vote is SUFFICIENT")
+
+console.log("---------------------------")
+
+// While vs for
+let viewedVideos = 0;
+let totalVideos = 3;
+while (viewedVideos < totalVideos) {
+    viewedVideos = viewedVideos + 1
+    console.log("[WHILE] Viewing video number", viewedVideos)
+}
+
+if (viewedVideos == totalVideos) {
+    console.log("Unit completed, viewed all videos\n")
+}
+
+for (let startingVideo = 1; startingVideo <= totalVideos; startingVideo++) {
+    console.log("[FOR] Viewing video number", startingVideo)
+    // viewVideos++ -> 2 at the first step, 3 at the second step, 4 at the third (4 <= 3 = false, so stop the cycle)
+}
+
+if (viewedVideos == totalVideos) {
+    console.log("Unit completed, viewed all videos\n")
+}

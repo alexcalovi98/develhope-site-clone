b5ef469ee517311de2708b64709e79db40d4a81e
Alex Calovi <alex.calovi98@gmail.com>
2026-05-07 18:58:59 +0200

    login script

---
+++ scripts/login.js
@@ -0,0 +1,41 @@
+// login(username, password) -> Promise (a buon fine oppure NON a buon fine)
+// se le credenziali corrispondono a alex.calovi@develhope.co e DVHPassword21!
+// la promise va risolta in maniera positiva restituendo un oggetto con nome e cognome dell'utente (Alex Calovi)
+// altrimenti un errore
+
+// login("alex.calovi@develhope.co", "DVHPassword21!")
+//     .then(result => console.log(result))
+//     .catch(error => console.log(error))
+//     .finally(() => console.log("Finally!"))
+
+// async/await
+
+tryLogin()
+
+async function tryLogin() {
+    try {
+        let result = await login("alex.calovi@develhope.co", "DVHPassword21!")
+        console.log(result)
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+console.log("Test")
+
+function login(username, password) {
+    return new Promise((resolve, reject) => {
+        if (username === "alex.calovi@develhope.co" && password === "DVHPassword21!") {
+            setTimeout(() => resolve({ name: "Alex", surname: "Calovi" }), 5000)
+        } else {
+            reject("Credentials are not correct")
+        }
+    })
+}
+
+//DB
+// 1 alex.calovi@develhope.co DVHPassword21!
+// 2 luca.verdi@develhope.co password1234!
+// 3 ...
+// 4 ...
+// ...

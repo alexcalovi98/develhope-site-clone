a3f44a88df36bbe8b26f43367c0afee21778e58c
Alex Calovi <alex.calovi98@gmail.com>
2026-06-23 18:49:10 +0200

    course card in react

---
+++ develhope-react/src/components/course-card/CourseCard.tsx
@@ -0,0 +1,30 @@
+import "./CourseCard.css";
+
+type CourseCardProps = {
+    image: string;
+    title: string;
+    bulletPoints: string[],
+    nextClass: Date,
+    discoverUrl: string
+}
+
+export function CourseCard({ image, title, bulletPoints, nextClass, discoverUrl }: CourseCardProps) {
+    const formattedNextClassDate = nextClass.toLocaleDateString("it-IT")
+
+    return (
+        <div className="course-card">
+            <img src={image} />
+            <div className="course-card-content">
+                <h2>{title}</h2>
+                <ul>
+                    <li>{bulletPoints[0]}</li>
+                    <li>{bulletPoints[1]}</li>
+                </ul>
+                <div className="next-class">Prossima classe: {formattedNextClassDate}</div>
+                <button>
+                    <span>Scopri di più</span>
+                </button>
+            </div>
+        </div>
+    )
+}
--- develhope-react/src/components/home-page/HomePage.tsx
+++ develhope-react/src/components/home-page/HomePage.tsx
@@ -1,11 +1,22 @@
+import { CourseCard } from "../course-card/CourseCard"
 import { Header } from "../header/Header"
 import { HomeBanner } from "../home-banner/HomeBanner"
 
 export function HomePage() {
+    const coursesBulletPoints = ["100% Online", "Inizia e studia quando vuoi"]
+    const now = new Date()
+
     return (
         <>
         <HomeBanner />
         <Header home={true} />
+        <CourseCard
+            image=""
+            title="Corso Live AI-Powered"
+            bulletPoints={coursesBulletPoints}
+            nextClass={now}
+            discoverUrl="" />
+        
         {/* <HelloWorld name="Alex" surname="Calovi" course="Mobile Development"/>
         <HelloWorld name="Rosario" surname="Floridia" age={27}/>
         <HelloWorld name="Federico Maria" surname="Calato" age={23}/> */}
